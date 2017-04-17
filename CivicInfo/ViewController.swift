ec376a9a90ea364c7af6f651f2234a590d11cb79
Michael Castro <castro.michael87@gmail.com>
2017-04-17 18:35:57 -0500

    Initial Commit

---
+++ CivicInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CivicInfo
+//
+//  Created by Michael Castro on 4/17/17.
+//  Copyright © 2017 Michael Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
